604073efda28b4361f7f64e3a73dcb9fedbc2185
Hector Ruiz Casbas <hruiz@poas.net>
2025-08-22 21:11:10 +0200

    Hola mundo

---
+++ deploy.bat
@@ -0,0 +1,24 @@
+@echo off
+set /p comentario="Introduce el comentario para el commit: "
+
+echo.
+echo ----------------------------------------
+echo Realizando git add...
+git add .
+
+echo.
+echo ----------------------------------------
+echo Realizando git commit...
+git commit -m "%comentario%"
+
+echo.
+echo ----------------------------------------
+echo Realizando git push...
+git push
+
+echo.
+echo ----------------------------------------
+echo ¡Despliegue completado!
+echo Accede a: https://atlas-mu-rust.vercel.app/
+
+pause

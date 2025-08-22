a7dcd77c5568b3d461133488c1f8ae03ef6098b9
Hector Ruiz Casbas <hruiz@poas.net>
2025-08-22 15:50:20 +0200

    Se cambia página inicio a LoginForm

--- Atlas-app/frontend/src/index.js
+++ Atlas-app/frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LoginForm from './components/LoginForm';
+import LoginForm from './frontend/src/components/LoginForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

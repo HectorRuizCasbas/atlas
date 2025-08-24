37dda6b13609a71be3e087e41591be8c067da9b4
HectorRuizCasbas <hruiz@zelenza.com>
2025-08-24 13:28:47 +0200

    prueba

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 
 // Importar todas las funciones de los módulos.
-import { showNewUserModal, hideNewUserModal, showUserCreatedSuccessModal } from './ui/modal.js';
+import { showNewUserModal, hideNewUserModal, showUserCreatedSuccessModal, hideUserCreatedSuccessModal } from './ui/modal.js';
 import { validatePasswordLength, validatePasswordMatch, transformUsernameToEmail } from './ui/validation.js';
 import { createUser } from './api/supabase.js';
 

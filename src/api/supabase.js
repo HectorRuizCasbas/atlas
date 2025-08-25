77c6c48baae9f33d5f2cb765a15d8af4312f9bf2
Hector Ruiz Casbas <hruiz@poas.net>
2025-08-25 08:17:41 +0200

    Prueba alta de usuario

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -2,8 +2,7 @@
 
 // URL del endpoint de la Función Edge. 
 // IMPORTANTE: Reemplaza <tu-referencia-proyecto> con tu referencia real de Supabase
-const SUPABASE_EDGE_FUNCTION_URL = 'https://<tu-referencia-proyecto>.supabase.co/functions/v1/create-user';
-
+const SUPABASE_EDGE_FUNCTION_URL = 'https://upbgpmcibngxukwaaiqh.supabase.co/functions/v1/create-user';
 /**
  * Envía una solicitud de creación de usuario a la función Edge de Supabase.
  * @param {object} userData Los datos del usuario (email, password, username).
